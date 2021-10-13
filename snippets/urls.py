4a3dc3484d3f13307742b764c0e30e17e79e8d57
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2021-10-13 22:06:27 +0100

    Auth done

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,8 +3,16 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path("snippets/", views.SnippetList.as_view()),
-    path("snippets/<int:pk>", views.SnippetDetail.as_view()),
+    path("", views.api_root),
+    path("snippets/", views.SnippetList.as_view(), name="snippet-list"),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snippet-detail"),
+    path(
+        "snippets/<int:pk>/hightlight/",
+        views.SnippetHighlight.as_view(),
+        name="snippet-highlight",
+    ),
+    path("users/", views.UserList.as_view(), name="user-list"),
+    path("users/<int:pk>/", views.UserDetail.as_view(), name="name-detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
